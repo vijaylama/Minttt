8286a7d6fbd0020b3e075d02f43cf57ad7c0586a
Vijay Lama <vijaylama2010@gmail.com>
2022-03-29 15:37:01 -0400

    Initial Commit

---
+++ Minttt/MintttApp.swift
@@ -0,0 +1,17 @@
+//
+//  MintttApp.swift
+//  Minttt
+//
+//  Created by Vijay Lama on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct MintttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
