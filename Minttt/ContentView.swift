662a848a01efb885239ca0a08de503848a43118a
Vijay Lama <vijaylama2010@gmail.com>
2022-03-30 17:26:14 -0400

    Charting added

--- Minttt/ContentView.swift
+++ Minttt/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 import SwiftUICharts
 
 struct ContentView: View {
-    var demoData: [Double] = [8, 2, 4, 6, 12, 9, 2]
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    
     var body: some View {
         NavigationView {
             ScrollView{
@@ -20,17 +21,21 @@ struct ContentView: View {
                         .bold()
                     
                     // MARK: Chart
-                    CardView {
-                        VStack {
-                            ChartLabel("$900", type: .title)
-                            LineChart()
+                    let data = transactionListVM.accumulateTransactions()
+                    if !data.isEmpty {
+                        let totalExpenses = data.last?.1 ?? 0
+                        CardView {
+                            VStack(alignment: .leading) {
+                                ChartLabel(totalExpenses.formatted(.currency(code: "USD")), type: .title, format: "$%.02f")
+                                LineChart()
+                            }
+                            .background(Color.systemBackground)
+                           
                         }
-                        .background(Color.systemBackground)
-                       
+                        .data(data)
+                        .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.4), Color.icon)))
+                        .frame(height: 300)
                     }
-                    .data(demoData)
-                    .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.4), Color.icon)))
-                    .frame(height: 300)
                     
                     // MARK: Transaction List
                     RecentTransactionList()
